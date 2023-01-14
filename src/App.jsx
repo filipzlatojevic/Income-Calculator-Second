61ad09b695917a087cef7aabe77de4df55b8de06
filipzlatojevic <ficozlatojevic@gmail.com>
2023-01-14 19:01:51 +0100

    feat(second calculator): create second tax calculator with different design but same template.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './app.css';
+import { Calculator } from './pages';
+
+const App = () => <Calculator />;
+
+export default App;
---
+++ src/components/Select.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Select = ({ value, onChange, moreClass }) => (
+  <select
+    value={value}
+    onChange={onChange}
+    className={`outline-none cursor-pointer focus:border-btnGreen focus:border focus:ring-btnGreen h-full 
+                    rounded-md bg-transparent py-0 sm:pl-2 sm:pr-1 text-gray-500 text-sm ${moreClass}`}>
+    <option value="1">Weekly</option>
+    <option value="2">Fortnightly</option>
+    <option value="4">Monthly</option>
+    <option value="48">Annually</option>
+  </select>
+);
+
+export default Select;
